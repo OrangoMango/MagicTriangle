e07ef9cc0eb0b2efadaa5a9fd9345084f994e6c0
Orango Mango <61402409+OrangoMango@users.noreply.github.com>
2020-09-26 19:29:44 +0200

    Add files via upload

--- app/src/com/orangomango/magictriangle/Main.java
+++ app/src/com/orangomango/magictriangle/Main.java
@@ -1,8 +1,13 @@
 package com.orangomango.magictriangle;
 
+import java.io.*;
+
 public class Main{
-        public static void main(String[] args){
-                Triangle triangle = new Triangle(6);
+        public static void main(String[] args) throws IOException{
+                System.out.print("Enter triangle lines: ");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+                Triangle triangle = new Triangle(Integer.parseInt(reader.readLine()));
                 
                 triangle.makeTriangle();
         }
