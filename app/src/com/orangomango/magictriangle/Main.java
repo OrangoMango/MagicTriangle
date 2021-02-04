4de17d40931e9fcbf4bf9981016e5a7db33ab280
Orango Mango <61402409+OrangoMango@users.noreply.github.com>
2021-02-04 08:43:20 +0100

    Update Main.java

--- app/src/com/orangomango/magictriangle/Main.java
+++ app/src/com/orangomango/magictriangle/Main.java
@@ -4,11 +4,15 @@ import java.io.*;
 
 public class Main{
         public static void main(String[] args) throws IOException{
-                System.out.print("Enter triangle lines: ");
-                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        
-                Triangle triangle = new Triangle(Integer.parseInt(reader.readLine()));
-                
-                triangle.makeTriangle();
+                if (args.length >= 1){
+                  Triangle triangle = new Triangle(Integer.parseInt(args[0]));
+                  triangle.makeTriangle();
+                }
+                else {
+                  System.out.print("Enter triangle lines: ");
+                  BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                  Triangle triangle = new Triangle(Integer.parseInt(reader.readLine()));
+                  triangle.makeTriangle();
+                }    
         }
 }
