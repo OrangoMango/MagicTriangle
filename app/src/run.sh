33f22e479f8bf0fed4d50916c89d41025dcddb89
OrangoMango <orapama06@gmail.com>
2021-02-04 07:58:25 +0000

    Added command line rgse args

--- app/src/run.sh
+++
@@ -1,5 +0,0 @@
-echo "Running in src the JAVA main class com.orangomango.magictriangle.Main"
-javac -d ../classes com/orangomango/magictriangle/*
-cd ../classes
-java com.orangomango.magictriangle.Main
-cd ../src
